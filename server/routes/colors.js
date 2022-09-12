869e7c1271695c8a7a221224da3fd233141526b5
ahmedmaher2481998 <ahmedmaher2481998@yahoo.com>
2022-09-12 16:57:15 +0200

    added bouns

--- server/routes/colors.js
+++ server/routes/colors.js
@@ -2,6 +2,16 @@ const express = require("express");
 
 const colorRouter = express.Router();
 
+colorRouter.post("/:name/css-styles", (req, res) => {
+	res.json({ msg: `"POST /colors/${req.params.name}/css-styles"` });
+});
+
+colorRouter.delete("/:name/css-styles/:style", (req, res) => {
+	res.json({
+		msg: `"Delete /colors/${req.params.name}/css-styles/${req.params.style}"`,
+	});
+});
+
 colorRouter.get("/:name", (req, res) => {
 	res.json({ msg: "Get colors/:name " + req.params.name });
 });
